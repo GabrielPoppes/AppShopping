279164b19988bf7c051a380bf6d28dad72f09a75
Gabriel Poppes <gabrielpoppes@outlook.com>
2021-08-04 08:34:25 -0300

    Finalizada a tela de detalhes

---
+++ AppShopping/AppShopping/Libraries/Converters/EstablishmentTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using AppShopping.Libraries.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppShopping.Libraries.Converters
+{
+    // Deixei a classe pública, e herdou de IValueConverter (implementando a interface)
+    public class EstablishmentTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            EstablishmentType type = (EstablishmentType)value;
+
+            // se o tipo for loja (store) retorna a palavra loja, do contrário, retorna restaurante
+            return (type == EstablishmentType.Store) ? "Loja" : "Restaurante";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppShopping/AppShopping/ViewModels/EstablishmentDetailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AppShopping.Libraries.Helpers.MVVM;
+using AppShopping.Models;
+using AppShopping.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppShopping.ViewModels
+{
+    // Classe pública e herdando de BaseViewModel
+    public class EstablishmentDetailViewModel : BaseViewModel
+    {
+        public Estabilishment Establishment { get; set; }
+        public EstablishmentDetailViewModel()
+        {
+            Establishment = new EstablishmentService().GetEstabilishments().First();
+        }
+    }
+}
